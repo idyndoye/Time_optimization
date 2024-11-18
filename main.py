0dadaf0f876c4b28e4a5c1798f57bf144b4bc174
idy <idy@wave.com>
2024-11-18 20:13:21 +0000

    creatig timer label

--- main.py
+++ main.py
@@ -20,11 +20,15 @@ window = Tk()
 window.title("time optimization")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+Title_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 50))
+Title_label.grid(column=1, row=0)
+
 canvas = Canvas(width=200, height=223, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image = tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
 
 
 window.mainloop()
